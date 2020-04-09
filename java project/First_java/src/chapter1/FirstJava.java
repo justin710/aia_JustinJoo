8670611afd98ede9d88d4f1dd8b38cde485a7c6c
justin710 <2689776@naver.com>
2020-04-09 10:32:47 +0900

    1교시

--- java project/First_java/src/chapter1/FirstJava.java
+++ java project/First_java/src/chapter1/FirstJava.java
@@ -5,7 +5,7 @@ public class FirstJava {
 	public static void main(String[] args) {
 
 
-		System.out.println("ThursDay");
+		System.out.println("gogogo!!!!");
 
 	}
 
---
+++ java project/exercise/src/ex1/Text1.java
@@ -0,0 +1,15 @@
+package ex1;
+
+public class Text1 {
+
+	public static void main(String[] args) {
+		/*
+		문제 1. 다음 두 문장을 출력하는 프로그램을 작성해 보자. 
+		System.out.println( “2+5=” + 2+5 ); System.out.println( “2+5=” + (2+5) );
+		 */
+	System.out.println("2+5=" + 2 + 5); // 25로 나옴
+	System.out.println("2+5=" + (2+5));
+	
+	}
+
+}
